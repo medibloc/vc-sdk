aeeff2a282f46269b5d329f5919d4adf4c722fd0
hansol-medi <hansol@medibloc.org>
2023-02-09 09:34:14 +0900

    tmp

--- pkg/vc/presentation_exchange_test.go
+++ pkg/vc/presentation_exchange_test.go
@@ -1,14 +1,16 @@
 package vc
 
 import (
+	"crypto/sha256"
 	"encoding/json"
-	"fmt"
+	"testing"
+	"time"
+
+	"github.com/hyperledger/aries-framework-go/pkg/crypto/primitive/bbs12381g2pub"
 	"github.com/hyperledger/aries-framework-go/pkg/doc/presexch"
 	"github.com/hyperledger/aries-framework-go/pkg/doc/util"
 	"github.com/hyperledger/aries-framework-go/pkg/doc/verifiable"
 	"github.com/stretchr/testify/require"
-	"testing"
-	"time"
 )
 
 var (
@@ -17,14 +19,27 @@ var (
 )
 
 func TestPresentationExchange(t *testing.T) {
+	bbsKeyType := "Bls12381G2Key2020"
+	bbsSigType := "BbsBlsSignature2020"
+	verificationMethod := "did:panacea:76e12ec712ebc6f1c221ebfeb1f#key1"
+
 	framework, err := NewFrameWork()
 	require.NoError(t, err)
 	loader := framework.loader
 
-	// valid credential
-	credValid := verifiable.Credential{
+	pubKey, privKey, err := bbs12381g2pub.GenerateKeyPair(sha256.New, nil)
+	require.NoError(t, err)
+
+	pubKeyBz, err := pubKey.Marshal()
+	require.NoError(t, err)
+
+	privKeyBz, err := privKey.Marshal()
+	require.NoError(t, err)
+
+	// verifiable credential
+	vc := verifiable.Credential{
 		ID:      "https://my-verifiable-credential.com",
-		Context: []string{verifiable.ContextURI},
+		Context: []string{verifiable.ContextURI, "https://w3id.org/security/bbs/v1"},
 		Types:   []string{verifiable.VCType},
 		Issuer: verifiable.Issuer{
 			ID: "did:panacea:76e12ec712ebc6f1c221ebfeb1f",
@@ -47,8 +62,18 @@ func TestPresentationExchange(t *testing.T) {
 		},
 	}
 
+	//vcByte, err := vc.MarshalJSON()
+	//require.NoError(t, err)
+	//
+	//signedVCByte, err := framework.SignCredential(vcByte, privKeyBz, &ProofOptions{
+	//	VerificationMethod: verificationMethod,
+	//	SignatureType:      bbsSigType,
+	//})
+	//require.NoError(t, err)
+	//
+	//signedVC, err := verifiable.ParseCredential(signedVCByte, )
+
 	required := presexch.Required
-	//preferred := presexch.Preferred
 
 	pd := &presexch.PresentationDefinition{
 		ID:      "c1b88ce1-8460-4baf-8f16-4759a2f055fd",
@@ -61,6 +86,9 @@ func TestPresentationExchange(t *testing.T) {
 			// https://github.com/hyperledger/aries-framework-go/commit/66d9bf30de2f5cd6116adaac27f277b45077f26f
 			Schema: []*presexch.Schema{{
 				URI:      "https://www.w3.org/2018/credentials#VerifiableCredential",
+				Required: false,
+			}, {
+				URI:      "https://w3id.org/security/bbs/v1",
 				Required: false,
 			}},
 			Constraints: &presexch.Constraints{
@@ -91,17 +119,57 @@ func TestPresentationExchange(t *testing.T) {
 	//loader, err := ldtestutil.DocumentLoader()
 	//require.NoError(t, err)
 
-	vp, err := pd.CreateVP([]*verifiable.Credential{&credValid}, loader, verifiable.WithJSONLDDocumentLoader(loader))
+	vp, err := pd.CreateVP([]*verifiable.Credential{&vc},
+		loader,
+		verifiable.WithJSONLDDocumentLoader(loader))
+	//verifiable.WithPublicKeyFetcher(verifiable.SingleKey(pubKeyBz, bbsKeyType))
 	require.NoError(t, err)
 
+	vp.Context = append(vp.Context, "https://w3id.org/security/bbs/v1")
+
 	vpBytes, err := json.MarshalIndent(vp, "", "\t")
+	//fmt.Println(string(vpBytes))
+
+	signedVP, err := framework.SignPresentation(vpBytes, privKeyBz, &ProofOptions{
+		VerificationMethod: verificationMethod,
+		SignatureType:      bbsSigType,
+		Domain:             "https://my-domain.com",
+		Challenge:          "challenge",
+		Created:            "2023-01-01T10:10:10Z",
+	})
+	require.NoError(t, err)
+
+	proofs, err := framework.GetPresentationProofs(signedVP)
+	require.NoError(t, err)
+	require.True(t, proofs.HasNext())
+
+	proof := proofs.Next()
+	require.NotNil(t, proof)
+	require.Equal(t, verificationMethod, proof.VerificationMethod)
+	require.Equal(t, bbsSigType, proof.Type)
+	require.Equal(t, "assertionMethod", proof.ProofPurpose)
+	require.Equal(t, "https://my-domain.com", proof.Domain)
+	require.Equal(t, "challenge", proof.Challenge)
+	require.Equal(t, "2023-01-01T10:10:10Z", proof.Created)
+	require.False(t, proofs.HasNext())
+	require.Nil(t, proofs.Next())
+
+	err = framework.VerifyPresentation(signedVP, pubKeyBz, bbsKeyType)
+	require.NoError(t, err)
+
+	//parsedVP, err := verifiable.ParsePresentation(signedVP, verifiable.WithPresPublicKeyFetcher(verifiable.SingleKey(pubKeyBz, bbsKeyType)))
+	//require.NoError(t, err)
+	//
+	//res, err := json.MarshalIndent(parsedVP, "", "\t")
+	//require.NoError(t, err)
+	//fmt.Println(string(res))
 
 	//fmt.Println(string(vpBytes))
 
-	pres, err := verifiable.ParsePresentation(vpBytes, verifiable.WithPresDisabledProofCheck(), verifiable.WithPresJSONLDDocumentLoader(loader))
-	require.NoError(t, err)
-
-	vpvp, err := json.MarshalIndent(pres, "", "\t")
-
-	fmt.Println(string(vpvp))
+	//pres, err := verifiable.ParsePresentation(vpBytes, verifiable.WithPublicKeyFetcher(verifiable.SingleKey(pubKeyBz, bbsKeyType)), verifiable.WithPresJSONLDDocumentLoader(loader))
+	//require.NoError(t, err)
+	//
+	//vpvp, err := json.MarshalIndent(pres, "", "\t")
+	//
+	//fmt.Println(string(vpvp))
 }
